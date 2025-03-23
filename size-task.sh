5eb3a36d9423af9a4ec426e223f4dc547f73c345
Omveer Panwar <109145701+Omveer99@users.noreply.github.com>
2025-03-24 02:12:03 +0530

    Create size-task.sh

---
+++ size-task.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+read -p "Your Path : " path
+cd "$path" || {echo "Wrong path;"}
+
+for file in *; do
+if [[-f "$file"]]; then
+size=$(du -sh "$file" | cut -f1)
+echo "$file -$size"
+fi
+done
